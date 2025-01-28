3844bab3eb3af36f12f4ed483765558f1aa6a26e
Albusinski <165928669+Albusinski@users.noreply.github.com>
2025-01-28 19:11:30 +0100

    file upload

---
+++ Structure&List/Structure&List/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Structure_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            textBox4 = new TextBox();
+            groupBox3 = new GroupBox();
+            button10 = new Button();
+            button9 = new Button();
+            textBox5 = new TextBox();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            label4 = new Label();
+            button4 = new Button();
+            button3 = new Button();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            statusStrip1 = new StatusStrip();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(439, 131);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Dane:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(218, 59);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Czyść";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(218, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Dodaj";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(80, 88);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(80, 59);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(80, 30);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Klasa:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 63);
+            label2.Name = "label2";
+            label2.Size = new Size(33, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Imię:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nazwisko:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Location = new Point(12, 149);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(199, 299);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Podgląd listy:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(6, 22);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(187, 271);
+            textBox4.TabIndex = 0;
+            
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button10);
+            groupBox3.Controls.Add(button9);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(button8);
+            groupBox3.Controls.Add(button7);
+            groupBox3.Controls.Add(button6);
+            groupBox3.Controls.Add(button5);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(button4);
+            groupBox3.Controls.Add(button3);
+            groupBox3.Location = new Point(217, 149);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(234, 299);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Opcje:";
+            // 
+            // button10
+            // 
+            button10.Location = new Point(52, 181);
+            button10.Name = "button10";
+            button10.Size = new Size(130, 23);
+            button10.TabIndex = 9;
+            button10.Text = "Wyczyść listę";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(52, 152);
+            button9.Name = "button9";
+            button9.Size = new Size(130, 23);
+            button9.TabIndex = 8;
+            button9.Text = "Usuń bieżący";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(81, 110);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(74, 23);
+            textBox5.TabIndex = 7;
+            textBox5.KeyUp += new KeyEventHandler(textBox5_KeyUp);
+            // 
+            // button8
+            // 
+            button8.Location = new Point(188, 110);
+            button8.Name = "button8";
+            button8.Size = new Size(40, 23);
+            button8.TabIndex = 6;
+            button8.Text = ">>";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(161, 110);
+            button7.Name = "button7";
+            button7.Size = new Size(21, 23);
+            button7.TabIndex = 5;
+            button7.Text = ">";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(52, 110);
+            button6.Name = "button6";
+            button6.Size = new Size(23, 23);
+            button6.TabIndex = 4;
+            button6.Text = "<";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(6, 110);
+            button5.Name = "button5";
+            button5.Size = new Size(40, 23);
+            button5.TabIndex = 3;
+            button5.Text = "<<";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(89, 92);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Wybrany:";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(52, 51);
+            button4.Name = "button4";
+            button4.Size = new Size(130, 23);
+            button4.TabIndex = 1;
+            button4.Text = "Pokaż bieżący";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(52, 22);
+            button3.Name = "button3";
+            button3.Size = new Size(130, 23);
+            button3.TabIndex = 0;
+            button3.Text = "Pokaż listę";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(13, 17);
+            toolStripStatusLabel1.Text = "0";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 451);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(463, 22);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            statusStrip1.ItemClicked += statusStrip1_ItemClicked;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(463, 473);
+            Controls.Add(statusStrip1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button2;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private GroupBox groupBox3;
+        private Button button3;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private StatusStrip statusStrip1;
+        private Button button10;
+        private Button button9;
+        private TextBox textBox5;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Label label4;
+        private Button button4;
+    }
+}
---
+++ Structure&List/Structure&List/Form1.cs
@@ -0,0 +1,161 @@
+namespace Structure_List
+{
+    public partial class Form1 : Form
+    {
+
+        private Dane d;
+        private List<Dane> list = new List<Dane>();
+        private int idBiezacy = 0;
+
+        struct Dane
+        {
+            public string nazwisko;
+            public string imie;
+            public string klasa;
+        }
+
+        private void dodaj()
+        {
+            czyscDana();
+            d.nazwisko = textBox1.Text;
+            d.imie = textBox2.Text;
+            d.klasa = textBox3.Text;
+            list.Add(d);
+            idBiezacy = list.Count - 1;
+        }
+
+        private void czyscDana()
+        {
+            d.nazwisko = "";
+            d.imie = "";
+            d.klasa = "";
+
+        }
+
+        private void czyscEdycje()
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+
+        }
+        private void pokazListe(TextBox tb)
+        {
+            tb.Clear();
+            for (int i = 0; i < list.Count; i++)
+            {
+                tb.AppendText((i + 1).ToString() + "********************" + Environment.NewLine);
+                tb.AppendText(list[i].nazwisko + Environment.NewLine);
+                tb.AppendText(list[i].imie + Environment.NewLine);
+                tb.AppendText(list[i].klasa + Environment.NewLine);
+
+
+            }
+        }
+
+        public void pokazRekord(int id, TextBox tb)
+        {
+            if (id < 0 || id > list.Count - 1) return;
+            d = list[id];
+            tb.Clear();
+            tb.AppendText((id + 1).ToString() + Environment.NewLine);
+            tb.AppendText(d.nazwisko + Environment.NewLine);
+            tb.AppendText(d.imie + Environment.NewLine);
+            tb.AppendText(d.klasa + Environment.NewLine);
+
+
+        }
+
+        public void pokazID(int id, TextBox tbID) {
+           
+            if (id < 0 || id > list.Count - 1) return;
+            d = list[id];
+            tbID.Clear();
+            tbID.AppendText((id + 1).ToString() + Environment.NewLine);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dodaj();
+            toolStripStatusLabel1.Text = list.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pokazListe(textBox4);
+            czyscEdycje();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            pokazRekord(idBiezacy, textBox4);
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            idBiezacy = 0;
+            pokazRekord(idBiezacy, textBox4);
+            pokazID(idBiezacy, textBox5);
+        }
+
+        // W lewo o jeden
+        public void button6_Click(object sender, EventArgs e) {
+            idBiezacy--;
+            if (idBiezacy < 0) idBiezacy = 0;
+            pokazRekord(idBiezacy, textBox4);
+            pokazID(idBiezacy, textBox5);
+        }
+
+        // W prawo o jeden
+        public void button7_Click(object sender, EventArgs e)
+        {
+            idBiezacy++;
+            if (idBiezacy < 0) idBiezacy = 0;
+            pokazRekord(idBiezacy, textBox4);
+            pokazID(idBiezacy, textBox5);
+        }
+
+
+        private void button9_Click(object sender, EventArgs e) {
+            list.RemoveRange(idBiezacy, 1);
+            toolStripStatusLabel1.Text = list.Count.ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e) { 
+            list.Clear(); 
+            toolStripStatusLabel1.Text = list.Count.ToString();
+            textBox4.Text = "";
+        }
+        private void textBox5_KeyUp(object sender, KeyEventArgs e) {
+
+            if (textBox5.Text.Length < 1)
+            {
+                idBiezacy = 0;
+            }
+            else { 
+                idBiezacy = Convert.ToInt16(textBox5.Text) - 1;
+            }
+            pokazRekord(idBiezacy, textBox4);
+
+         
+            
+        }
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
